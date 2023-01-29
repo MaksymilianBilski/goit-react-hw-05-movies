e64b36ca3e4d2b3fd0d52ed9b9341d14382c970c
Maksymilian Bilski <biger9@o2.pl>
2023-01-29 11:20:07 +0100

    react lazy

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
-import MovieDetails from 'pages/MovieDetails/MovieDetails';
+import { lazy, Suspense } from 'react';
 import Button from './components/Button/Button';
-import TrendingList from 'pages/Home/Home';
-import MovieCast from 'pages/MovieCast/MovieCast';
-import Reviews from 'pages/Reviews/Reviews';
-import SearchMovies from 'pages/SearchMovies/Movies';
+// import MovieDetails from 'pages/MovieDetails/MovieDetails';
+// import TrendingList from 'pages/Home/Home';
+// import MovieCast from 'pages/MovieCast/MovieCast';
+// import Reviews from 'pages/Reviews/Reviews';
+// import SearchMovies from 'pages/SearchMovies/Movies';
+
+const MovieDetails = lazy(() => import('pages/MovieDetails/MovieDetails'));
+const TrendingList = lazy(() => import('pages/Home/Home'));
+const MovieCast = lazy(() => import('pages/MovieCast/MovieCast'));
+const Reviews = lazy(() => import('pages/Reviews/Reviews'));
+const SearchMovies = lazy(() => import('pages/SearchMovies/Movies'));
 
 const App = () => {
   return (
@@ -22,22 +29,23 @@ const App = () => {
         <Button path="/goit-react-hw-05-movies" name="home" />
         <Button path="/movies" name="movies" />
       </div>
-      <Routes>
-        <Route
-          path="/goit-react-hw-05-movies"
-          element={<TrendingList />}
-        />
-        <Route path="/movies" element={<SearchMovies />} />
-        <Route path="/movies/:movieId" element={<MovieDetails />}>
-          <Route path="cast" element={<MovieCast />}/>
-          <Route path="reviews" element={<Reviews />}/>
-        </Route>
+      <Suspense
+        fallback={<div style={{ height: '200', fontSize: 50 }}>loading...</div>}
+      >
+        <Routes>
+          <Route path="/goit-react-hw-05-movies" element={<TrendingList />} />
+          <Route path="/movies" element={<SearchMovies />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<MovieCast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
 
-        <Route
-          path="*"
-          element={<Navigate to="/goit-react-hw-05-movies" />}
-        ></Route>
-      </Routes>
+          <Route
+            path="*"
+            element={<Navigate to="/goit-react-hw-05-movies" />}
+          ></Route>
+        </Routes>
+      </Suspense>
     </div>
   );
 };
